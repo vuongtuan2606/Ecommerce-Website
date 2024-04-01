626febd83518eb93ac2546df1d1af3bc14fbfda1
tuanvuong2606 <tuanvuong260621@gmail.com>
2024-04-01 09:13:35 +0700

    first commit

---
+++ src/main/java/com/tuanvuong/qtsnearker/QtsnearkerApplication.java
@@ -0,0 +1,13 @@
+package com.tuanvuong.qtsnearker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtsnearkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QtsnearkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuanvuong/qtsnearker/QtsnearkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuanvuong.qtsnearker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QtsnearkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
